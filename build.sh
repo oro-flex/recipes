d35d127b5f09ab5c9ed6c240cf7ea5fbe729c278
Nikita Makarov <mesaverde228@gmail.com>
2021-12-29 21:08:12 +0300

    Added buils script

---
+++ build.sh
@@ -0,0 +1,20 @@
+mkdir -p .build && cd .build
+# wget -q -O recipes-checker.zip https://codeload.github.com/symfony-tools/recipes-checker/zip/refs/heads/main
+curl https://codeload.github.com/symfony-tools/recipes-checker/zip/refs/heads/main -o recipes-checker.zip
+unzip recipes-checker.zip
+cd recipes-checker-main
+composer install --ansi --no-dev
+cd ..
+cd ..
+rm -rf flex-endpoint && mkdir -p flex-endpoint
+git ls-tree HEAD */*/* | php .build/recipes-checker-main/run generate:flex-endpoint oro-flex/recipes master flex/main flex-endpoint
+rm -rf .build
+git checkout flex/main
+git rm -q *.json
+mv flex-endpoint/*.json .
+git add *.json
+cp -a flex-endpoint/archived .
+git add archived/
+git commit -m 'Update Flex endpoint' || true
+rm -rf flex-endpoint
+# git push origin -f flex/main
